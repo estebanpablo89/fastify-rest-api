e31438ea471fc3d77a740ec565110fd22165d472
estebanpablo89 <estebanpablo89@gmail.com>
2021-08-09 13:05:58 -0500

    routes and controllers

---
+++ routes/items.js
@@ -0,0 +1,45 @@
+const { getItems, getItem } = require('../controllers/items');
+
+// Item schema
+
+const Item = {
+  type: 'object',
+  properties: {
+    id: { type: 'string' },
+    name: { type: 'string' },
+  },
+};
+
+// options for get all items
+const getItemsOpts = {
+  schema: {
+    response: {
+      200: {
+        type: 'array',
+        items: Item,
+      },
+    },
+  },
+  handler: getItems,
+};
+
+// options for get single item
+const getItemOpts = {
+  schema: {
+    response: {
+      200: Item,
+    },
+  },
+  handler: getItem,
+};
+
+function itemRoutes(fastify, options, done) {
+  // get all items
+  fastify.get('/items', getItemsOpts);
+
+  // get single item
+  fastify.get('/items/:id', getItemOpts);
+  done();
+}
+
+module.exports = itemRoutes;
--- server.js
+++ server.js
@@ -1,16 +1,7 @@
 const fastify = require('fastify')({ logger: true });
 const PORT = 5000;
 
-const items = require('./Items');
-
-fastify.get('/items', (req, reply) => {
-  reply.send(items);
-});
-fastify.get('/items/:id', (req, reply) => {
-  const { id } = req.params;
-  const item = items.find(item => item.id === id);
-  reply.send(item);
-});
+fastify.register(require('./routes/items'));
 
 const start = async () => {
   try {
