18cbdf7cea99658dc5879d8e36409c7e0811b6ef
estebanpablo89 <estebanpablo89@gmail.com>
2021-08-09 13:16:00 -0500

    add item

--- routes/items.js
+++ routes/items.js
@@ -1,4 +1,8 @@
-const { getItems, getItem } = require('../controllers/items');
+const {
+  getItems,
+  getItem,
+  addItem,
+} = require('../controllers/items');
 
 // Item schema
 
@@ -33,12 +37,26 @@ const getItemOpts = {
   handler: getItem,
 };
 
+// options for add item
+const postItemOpts = {
+  schema: {
+    response: {
+      201: Item,
+    },
+  },
+  handler: addItem,
+};
+
 function itemRoutes(fastify, options, done) {
   // get all items
   fastify.get('/items', getItemsOpts);
 
   // get single item
   fastify.get('/items/:id', getItemOpts);
+
+  // add item
+  fastify.post('/items', postItemOpts);
+
   done();
 }
 
